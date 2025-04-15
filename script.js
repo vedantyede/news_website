d3d505a95f96178f2d11b1b111d4ad88cd9b8969
unknown <vedantyede@gmail.com>
2025-04-15 15:16:27 +0530

    Add initial HTML, CSS, and JavaScript files for travel articles display

---
+++ script.js
@@ -0,0 +1,57 @@
+async function fetchData() {
+  let res = await fetch("./data.json");
+  let data = await res.json();
+  console.log(data);
+  console.log(data.articles);
+  showData(data);
+}
+
+function showData(data) {
+  const ul = document.getElementById("card-container");
+
+  const title = document.getElementById("title");
+  title.innerHTML = `${data.title} >`;
+
+  data.articles.map((el) => {
+    let li = document.createElement("li");
+    li.classList.add("card");
+
+    let seperator = document.createElement("div");
+    seperator.classList.add("seperator");
+
+    let category = document.createElement("h5");
+    category.classList.add("card-category");
+    category.innerHTML = `${el.primarySectionRouteName}`;
+
+    let headline_a = document.createElement("a");
+    headline_a.classList.add("card-headline__link");
+    headline_a.href = `${el.link}`;
+    headline_a.innerHTML = `${el.headline}`;
+
+    let headline = document.createElement("h4");
+    headline.classList.add("card-headline");
+    headline.appendChild(headline_a);
+
+    let image = document.createElement("img");
+    image.classList.add("card-image");
+    image.src = `${el.thumbnail.src}`;
+
+    let para = document.createElement("p");
+    para.classList.add("card-standfirst");
+    para.innerHTML = `${el.standfirst}`;
+
+    let card_detail = document.createElement("div");
+    card_detail.classList.add("card-detail");
+
+    card_detail.appendChild(headline);
+    card_detail.appendChild(image);
+    card_detail.appendChild(para);
+
+    li.appendChild(category);
+    li.appendChild(card_detail);
+    li.appendChild(seperator);
+    ul.appendChild(li);
+  });
+}
+
+fetchData();
