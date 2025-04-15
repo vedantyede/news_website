cf3e18181fb4ab2740a1c8eb90d2bacf5d43c4c5
unknown <vedantyede@gmail.com>
2025-04-15 16:47:59 +0530

    Refactor variable declarations in fetchData and showData functions to use const for better readability and performance

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function fetchData() {
   try {
-    let res = await fetch("./data.json");
-    let data = await res.json();
+    const res = await fetch("./data.json");
+    const data = await res.json();
     showData(data);
   } catch (err) {
     console.log("Error: ", err);
@@ -15,34 +15,34 @@ function showData(data) {
   title.innerHTML = `${data.title} >`;
 
   data.articles.map((el) => {
-    let li = document.createElement("li");
+    const li = document.createElement("li");
     li.classList.add("card");
 
-    let seperator = document.createElement("div");
+    const seperator = document.createElement("div");
     seperator.classList.add("seperator");
 
-    let category = document.createElement("h5");
+    const category = document.createElement("h5");
     category.classList.add("card-category");
     category.innerHTML = `${el.primarySectionRouteName}`;
 
-    let headline_a = document.createElement("a");
+    const headline_a = document.createElement("a");
     headline_a.classList.add("card-headline__link");
     headline_a.href = `${el.link}`;
     headline_a.innerHTML = `${el.headline}`;
 
-    let headline = document.createElement("h4");
+    const headline = document.createElement("h4");
     headline.classList.add("card-headline");
     headline.appendChild(headline_a);
 
-    let image = document.createElement("img");
+    const image = document.createElement("img");
     image.classList.add("card-image");
     image.src = `${el.thumbnail.src}`;
 
-    let para = document.createElement("p");
+    const para = document.createElement("p");
     para.classList.add("card-standfirst");
     para.innerHTML = `${el.standfirst}`;
 
-    let card_detail = document.createElement("div");
+    const card_detail = document.createElement("div");
     card_detail.classList.add("card-detail");
 
     card_detail.appendChild(headline);
